6f19fec16b36f0cd01912bd89e39ff49c1df31e2
03OCTMWFWD-Divyesh <gajnotardivyesh@gmail.com>
2023-05-31 15:29:37 +0530

    Jquery

---
+++ Assessment/script.js
@@ -0,0 +1,12 @@
+function one() {
+    document.getElementById("slide-img").src = "https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+}
+function two() {
+    document.getElementById("slide-img").src = "https://images.pexels.com/photos/3075993/pexels-photo-3075993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+}
+function three() {
+    document.getElementById("slide-img").src = "https://images.pexels.com/photos/4119162/pexels-photo-4119162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+}
+function four() {
+    document.getElementById("slide-img").src = "https://images.pexels.com/photos/5179997/pexels-photo-5179997.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+}
